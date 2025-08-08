f0a96a6d9a0267398cab9551a14cef9df5529988
Pinkey Rani <pinkeyrani894@gmail.com>
2025-08-08 22:10:44 +0600

    exam question 3

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const header = document.querySelectorAll(".header");
+const details = document.querySelectorAll(".details");
+
+header.forEach((head, ind) => {
+    head.addEventListener("click", () => {
+        details.forEach((d, i) => {
+            if (ind == i) {
+                details[ind].style.display = details[ind].style.display == "block" ? "none" : "block";
+            }
+            else {
+                details[i].style.display = "none";
+            }
+        })
+
+    })
+
+})
+
+const bttn = document.querySelectorAll(".bttn");
+const sent = document.querySelectorAll(".sent");
+sent[0].style.display ="block" ;
+bttn[0].style.backgroundColor = "#ede435ff"; 
+
+bttn.forEach((headd, nd) => {
+ headd.addEventListener("click",() => {
+
+    sent.forEach((dd, s) => {
+        if(nd == s){
+             sent[nd].style.display ="block" ;
+             bttn[nd].style.backgroundColor = "#ede435ff"; 
+
+        }
+        else{
+            sent[s].style.display ="none";
+            bttn[s].style.backgroundColor = "#bef576"; 
+
+        }
+    })
+    })
+})
+const hour = document.querySelectorAll(".hrs12")
+const time = document.getElementById("time");
+const showTime = () =>{
+const d = new Date();
+let hrs = d.getHours();
+const ampm = hrs >= 12 ? "PM":"AM";
+ hrs = hrs > 12? hrs-12 :hrs ;
+
+
+
+time.innerText = hrs + ":"+ d.getMinutes() + ":" + d.getSeconds() +" "+ ampm;
+console.log(d.getHours()+ ":"+ d.getMinutes() + ":" + d.getSeconds() )
+}
+showTime();
+setInterval(showTime, 1000);
+
+
